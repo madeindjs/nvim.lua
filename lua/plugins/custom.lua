87ba417cbe3f2048d67a408bbcc0fdc16a59b9cd
Alexandre Rousseau <alexandre@rsseau.fr>
2024-08-08 23:36:53 +0200

    add suround

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -15,4 +15,15 @@ return {
   {
     "almo7aya/openingh.nvim",
   },
+  -- use cs"' to change quotes
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
